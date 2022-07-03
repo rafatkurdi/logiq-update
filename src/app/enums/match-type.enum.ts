ba114ea5b918582240a786c21efd1e751e587e03
rafat <rafat@rafats-MacBook-Pro.local>
2022-07-03 11:51:48 +0200

    first commit

---
+++ src/app/enums/match-type.enum.ts
@@ -0,0 +1,37 @@
+export enum MatchTypes {
+    SHOT = 'shot',
+    PASS = 'pass',
+    PUCKWON = 'puckWon',
+    ZONE_ENTRY = 'zoneEntry',
+    ZONE_EXIT = 'zoneExit',
+    DUMP_OUT = 'dumpOut',
+    OFFENSIVE_ZONE_LOSS = 'offensiveZoneLoss',
+    HIT = 'hit',
+    SHOOTOUT = 'shootout',
+    PENALTY = 'penalty'
+}
+
+export enum EventType {
+    SHOT = 'STŘELA',
+    PASS = 'PŘIHŘÁVKA',
+    PUCKWON = 'ZISK PUKU',
+    ZONEENTRY = 'VSTUP DO PÁSMA',
+    ZONEEXIT = 'VÝSTUP Z PÁSMA',
+    DUMPOUT = 'VYHOZENÍ',
+    DUMPIN = 'NAHOZENÍ',
+    SHIFT = 'STŘÍDÁNÍ',
+    THROWING = 'NESPECIFIKOVANE',
+    FACEOFF = 'VHAZOVÁNÍ',
+    HIT = 'SRÁŽKA'
+}
+
+
+
+export enum EventKey {
+    SHOTS = 'strela',
+    PASSES = 'prihravky',
+    ENTRIES = 'vstupy_do_pasma',
+    EXITS = 'vystupy_z_pasma',
+    SHOOTOUTS = "najezdy" ,
+    FACEOFFS = 'vhazovani',
+}
